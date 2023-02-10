133b425acb5f9cb4bd05c22a32658833f55bc527
Jaden Bertino <jaden@bertinofamily.com>
2023-02-10 14:11:14 -0800

    create basic html css for sign up / login sign out

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,8 +4,15 @@ export default function Nav() {
   return (
     <nav>
       <div className="container">
-        <span>Code Journal</span>
-        <button className="btn">Entries</button>
+        <div className="title">
+          <h3>Code Journal</h3>
+          <button className="btn">Entries</button>
+        </div>
+        <div className="auth">
+          <button className="btn sign-up">Sign Up</button>
+          <button className="btn log-in">Log In</button>
+          <button className="btn sign-out">Sign Out</button>
+        </div>
       </div>
     </nav>
   );
