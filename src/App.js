aaf4ad8e024465c223a254df3042e5702fe70405
Jaden Bertino <jaden@bertinofamily.com>
2023-02-09 20:42:40 -0800

    create navbar

--- src/App.js
+++ src/App.js
@@ -3,12 +3,33 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 // pages
 import { CreateEntry, EditEntry, ViewEntries } from './pages/pages'
 
+// components
+import { Nav } from './components/components'
+
 // styles
 import './App.css';
 
 function App() {
+
+  // Import Data from local storage on load
+  const prevData = localStorage.getItem('dataJSON')
+  const data = prevData ? JSON.parse(prevData) : {
+    view: 'entry-form',
+    entries: [],
+    editing: null,
+    nextEntryId: 1,
+  }
+
+  // Export Data to local storage on exit
+  window.addEventListener('beforeunload', () => {
+    const dataJSON = JSON.stringify(data)
+    localStorage.setItem('dataJSON', dataJSON)
+  })
+  
   return (
     <Router>
+      <Nav />
+
       <Routes>
         <Route path="/" element={<ViewEntries />}/>
         <Route path="/create" element={<CreateEntry />}/>
---
+++ src/components/Nav.jsx
@@ -0,0 +1,12 @@
+import "./Nav.css";
+
+export default function Nav() {
+  return (
+    <nav>
+      <div className="container">
+        <span>Code Journal</span>
+        <button className="btn">Entries</button>
+      </div>
+    </nav>
+  );
+}
