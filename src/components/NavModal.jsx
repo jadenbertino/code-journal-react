656cf31cc629a13c9a57421d404fd943bea807d4
Jaden Bertino <jaden@bertinofamily.com>
2023-02-12 14:49:14 -0800

    create logic for each button on nav menu

--- src/components/NavModal.jsx
+++ src/components/NavModal.jsx
@@ -1,33 +1,51 @@
 import ReactDOM from 'react-dom';
 import { useAuthContext } from '../hooks/useAuthContext';
 import { Link } from 'react-router-dom';
+import { useLogOut } from '../hooks/useLogOut';
+import { useNavigate } from 'react-router-dom';
 
 // styles
 import './NavModal.css'
 
 export default function NavModal({setModalActive}) {
   const { user } = useAuthContext()
-
+  const { logout } = useLogOut()
   const root = document.querySelector('#root');
+  const nav = useNavigate()
 
+  async function handleLogOut() {
+    await logout()
+    setModalActive(false)
+  }
+
+  function closeModal() {
+    setModalActive(false)
+  }
+  
   return ReactDOM.createPortal((
     <div className="modal-backdrop">
       <div className="modal nav-modal">
         <div className="modal-content-wrapper">
           <h2>Menu</h2>
-          <Link>
+          <Link to="/">
             <button className="btn">View Entries</button>
           </Link>
-          <Link>
+          <Link to="/create">
             <button className="btn">Create New Entry</button>
           </Link>
-          {user && <button className="btn">Sign Out</button>}
-          {!user && (<>
-            <button className="btn">Log In</button>
-            <button className="btn">Sign Out</button>
-          </>)}
+          {user && <button className="btn" onClick={handleLogOut}>Sign Out</button>}
+          {!user && (
+            <>
+              <Link to="/login">
+                <button className="btn" onClick={closeModal}>Log In</button>
+              </Link>
+              <Link to="/signup">
+                <button className="btn" onClick={closeModal}>Sign Up</button>
+              </Link>
+            </>
+          )}
           <button className="btn btn-sm exit-modal-btn">
-            <i class="fa-solid fa-x" onClick={() => setModalActive(false)}></i>
+            <i class="fa-solid fa-x" onClick={closeModal}></i>
           </button>
         </div>
       </div>
