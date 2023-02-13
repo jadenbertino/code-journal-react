c4bd614a03de256530422542d9b03d299e9c4ab4
Jaden Bertino <jaden@bertinofamily.com>
2023-02-13 10:53:53 -0800

    display timeCreated on entry

--- src/pages/view/RenderEntries.jsx
+++ src/pages/view/RenderEntries.jsx
@@ -28,35 +28,55 @@ export default function RenderEntries({ entries, searchQuery, resetQuery }) {
     }))
   }, [searchQuery])
 
+  function formatTimestamp(timestamp) {
+    // thank you chatgpt
+    const timeCreated = new Date(timestamp.seconds * 1000)
+    return timeCreated.toLocaleDateString("en-US", {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: true
+    });
+    
+  }
+
   return (
     <>
-      {queriedEntries.length ? <ul>
-        {queriedEntries.map(entry => (<li className="entry" key={entry.id}>
-          <div className="img-wrapper">
-            <img src={entry.imgSrc} alt="entry-img" />
-          </div>
-          <div className="text-wrapper">
-            <div className="title-wrapper">
-              <h3 className="entry-title">{entry.title}</h3>
-              <Link to={`/edit/${entry.id}`}>
-                <button className="btn pencil">
-                  <i className="fa-solid fa-pencil"></i>
-                </button>
-              </Link>
-            </div>
-            <p>{entry.notes}</p>
-          </div>
-        </li>))}
-      </ul> : (
+      {queriedEntries.length ? 
+        <ul>
+          {queriedEntries.map(entry => (
+            <li className="entry" key={entry.id}>
+              <div className="img-wrapper">
+                <img src={entry.imgSrc} alt="entry-img" />
+              </div>
+              <div className="text-wrapper">
+                <div className="header-wrapper">
+                  <div className="title-wrapper">
+                    <h3 className="entry-title">{entry.title}</h3>
+                    <Link to={`/edit/${entry.id}`}>
+                      <button className="btn pencil">
+                        <i className="fa-solid fa-pencil"></i>
+                      </button>
+                    </Link>
+                  </div>
+                  <p className="entry-datetime">{formatTimestamp(entry.timeCreated)}</p>
+                </div>
+                <p className="entry-notes">{entry.notes}</p>
+              </div>
+            </li>
+          ))}
+        </ul> : (
         <div className="no-entries">
-        <p>{`No entries${entries.length ? " match this query" : ""}`}</p>
-        <div className="btns-wrapper">
-          {entries.length ? <button className="btn" onClick={resetQuery}>Reset Query</button> : null}
-          <Link to="/create">
-            <button className="btn">Create New Entry</button>
-          </Link>
-        </div>
-      </div> 
+          <p>{`No entries${entries.length ? " match this query" : ""}`}</p>
+          <div className="btns-wrapper">
+            {entries.length ? <button className="btn" onClick={resetQuery}>Reset Query</button> : null}
+            <Link to="/create">
+              <button className="btn">Create New Entry</button>
+            </Link>
+          </div>
+        </div> 
       )}
     </>
   )
